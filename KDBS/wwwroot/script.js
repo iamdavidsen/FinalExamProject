d1368af9ca377fe7d5bc5a66a7f63ffd8aa27837
Rasmus <Rasmus@davidsen.biz>
2021-11-02 10:09:19 +0000

    Merged PR 29: Added map
    Added map

---
+++ KDBS/wwwroot/script.js
@@ -0,0 +1,24 @@
+﻿function loadBingMap(key) {
+    var map = new Microsoft.Maps.Map('#map', {
+        credentials: key,
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        zoom: 10
+    });
+    
+    window.map = map;
+    
+    return "";
+}
+
+function addPins(coordinates) {
+    window.map.entities.deleteAllShapes()
+   
+    for (var i = 0; i < coordinates.length; i++) {
+        var coordinate = coordinates[i];
+        
+        var pushpin = new Microsoft.Maps.Pushpin(coordinate, null);
+        window.map.entities.push(pushpin);
+    }
+    
+    return "";
+}
